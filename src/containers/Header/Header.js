596f6761c4c0153c37ca4a29cb1436827253d00d
Katelyn Marsala <katelyn_marsala@homedepot.com>
2019-09-03 15:56:06 -0400

    NavBar media queries

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import TmdbIcon from '../../assets/tmdbIcon.png';
 import './Header.scss';
 
-function Header() {
+const Header = () => {
+  const [expandedLinks, setExpandedLinks] = useState(false);
+
+  const toggleLinks = () => () => {
+    setExpandedLinks(!expandedLinks)
+  }
+
   return (
-    <nav className="navbar-wrapper">
-      <ul className="navbar-links">
-        <li className="navbar-link">
-          <NavLink to="/">
-            <img src={TmdbIcon} alt="logo" />
-          </NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/nowplaying">Now Playing</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/upcoming">Coming Soon</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/search">Movie Search</NavLink>
-        </li>
-        <li className="navbar-link">
-          <NavLink to="/genres">Genres</NavLink>
-        </li>
-      </ul>
-    </nav>
+      <nav className="navbar-wrapper">
+        <ul className="navbar-links">
+          <div>
+            <li className="navbar-link logo" onClick={() => setExpandedLinks(false)}>
+            <NavLink to="/" >
+              <img src={TmdbIcon} alt="logo" />
+            </NavLink>
+            </li>
+            <li className="navbar-link bars" onClick={toggleLinks()}>
+              <i className="fa fa-bars" aria-hidden="true" />
+            </li>
+          </div>
+          <div className={expandedLinks ? '' : 'hidden'}>
+            <li className="navbar-link" onClick={() => setExpandedLinks(false)}>
+              <NavLink to="/nowplaying">Now Playing</NavLink>
+            </li>
+            <li className="navbar-link" onClick={() => setExpandedLinks(false)}>
+              <NavLink to="/upcoming">Coming Soon</NavLink>
+            </li>
+            <li className="navbar-link" onClick={() => setExpandedLinks(false)}>
+              <NavLink to="/search">Movie Search</NavLink>
+            </li>
+            <li className="navbar-link" onClick={() => setExpandedLinks(false)}>
+              <NavLink to="/genres">Genres</NavLink>
+            </li>
+          </div>
+        </ul>
+      </nav>
   );
 }
 
